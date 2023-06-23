ec80313a7b7d93e2aeefd20ed7f98afbc2370cfb
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-06-23 12:56:03 +0200

    First draft of dfgviewer xsd and generated model classes

---
+++ libmeta-dfgviewer/src/main/java/org/mycore/libmeta/dfgviewer/model/Links.java
@@ -0,0 +1,248 @@
+//
+// Diese Datei wurde mit der Eclipse Implementation of JAXB, v3.0.0 generiert 
+// Siehe https://eclipse-ee4j.github.io/jaxb-ri 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2023.06.23 um 12:38:43 PM CEST 
+//
+
+
+package org.mycore.libmeta.dfgviewer.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>Java-Klasse für anonymous complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="reference" maxOccurs="unbounded"&gt;
+ *           &lt;complexType&gt;
+ *             &lt;simpleContent&gt;
+ *               &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema&gt;string"&gt;
+ *                 &lt;attribute name="linktext" use="required" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+ *               &lt;/extension&gt;
+ *             &lt;/simpleContent&gt;
+ *           &lt;/complexType&gt;
+ *         &lt;/element&gt;
+ *         &lt;element name="presentation" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="sru" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="iiif" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "reference",
+    "presentation",
+    "sru",
+    "iiif"
+})
+@XmlRootElement(name = "links")
+public class Links {
+
+    @XmlElement(required = true)
+    protected List<Links.Reference> reference;
+    protected String presentation;
+    protected String sru;
+    protected String iiif;
+
+    /**
+     * Gets the value of the reference property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the Jakarta XML Binding object.
+     * This is why there is not a <CODE>set</CODE> method for the reference property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getReference().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Links.Reference }
+     * 
+     * 
+     */
+    public List<Links.Reference> getReference() {
+        if (reference == null) {
+            reference = new ArrayList<Links.Reference>();
+        }
+        return this.reference;
+    }
+
+    /**
+     * Ruft den Wert der presentation-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPresentation() {
+        return presentation;
+    }
+
+    /**
+     * Legt den Wert der presentation-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPresentation(String value) {
+        this.presentation = value;
+    }
+
+    /**
+     * Ruft den Wert der sru-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSru() {
+        return sru;
+    }
+
+    /**
+     * Legt den Wert der sru-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSru(String value) {
+        this.sru = value;
+    }
+
+    /**
+     * Ruft den Wert der iiif-Eigenschaft ab.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIiif() {
+        return iiif;
+    }
+
+    /**
+     * Legt den Wert der iiif-Eigenschaft fest.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIiif(String value) {
+        this.iiif = value;
+    }
+
+
+    /**
+     * <p>Java-Klasse für anonymous complex type.
+     * 
+     * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+     * 
+     * <pre>
+     * &lt;complexType&gt;
+     *   &lt;simpleContent&gt;
+     *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema&gt;string"&gt;
+     *       &lt;attribute name="linktext" use="required" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+     *     &lt;/extension&gt;
+     *   &lt;/simpleContent&gt;
+     * &lt;/complexType&gt;
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "value"
+    })
+    public static class Reference {
+
+        @XmlValue
+        protected String value;
+        @XmlAttribute(name = "linktext", required = true)
+        protected String linktext;
+
+        /**
+         * Ruft den Wert der value-Eigenschaft ab.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getValue() {
+            return value;
+        }
+
+        /**
+         * Legt den Wert der value-Eigenschaft fest.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        /**
+         * Ruft den Wert der linktext-Eigenschaft ab.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getLinktext() {
+            return linktext;
+        }
+
+        /**
+         * Legt den Wert der linktext-Eigenschaft fest.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setLinktext(String value) {
+            this.linktext = value;
+        }
+
+    }
+
+}
---
+++ libmeta-dfgviewer/src/main/java/org/mycore/libmeta/dfgviewer/model/package-info.java
@@ -0,0 +1,13 @@
+//
+// Diese Datei wurde mit der Eclipse Implementation of JAXB, v3.0.0 generiert 
+// Siehe https://eclipse-ee4j.github.io/jaxb-ri 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2023.06.23 um 12:38:43 PM CEST 
+//
+
+@XmlSchema(namespace = "http://dfg-viewer.de/DV/", xmlns = {
+    @XmlNs(namespaceURI = "http://dfg-viewer.de/DV/", prefix = "dv") }, elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.mycore.libmeta.dfgviewer.model;
+
+import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlSchema;
