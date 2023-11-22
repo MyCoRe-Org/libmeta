8a2ecb2ac28d83afcefc498a70c5327a2f75a232
Robert Stephan <robert.stephan@uni-rostock.de>
2023-11-22 16:14:57 +0100

    initial checkin for DAIA 1.0 #15

---
+++ libmeta-daia__1_0/src/main/java/org/mycore/libmeta/daia__1_0/DAIAJSONProcessor.java
@@ -0,0 +1,89 @@
+/* 
+ * This file is part of *** MyCoRe LibMeta ***
+ * See https://github.com/MyCoRe-Org/libmeta/ for details.
+ * 
+ * MyCoRe LibMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * MyCoRe LibMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyCoRe LibMeta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.libmeta.daia__1_0;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.mycore.libmeta.daia__1_0.model.DAIA;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbConfig;
+
+/**
+ * @author Robert Stephan
+ */
+public class DAIAJSONProcessor {
+
+    private static final DAIAJSONProcessor INSTANCE = new DAIAJSONProcessor();
+    
+    private JsonbConfig cfg = new JsonbConfig().withFormatting(true);
+    
+
+    //private constructor to avoid client applications to use constructor
+    private DAIAJSONProcessor() {
+    }
+
+    public static DAIAJSONProcessor getInstance() {
+        return INSTANCE;
+    }
+
+    public void marshal(DAIA daia, Path p) throws Exception {
+        try (BufferedWriter bw = Files.newBufferedWriter(p)){
+            Jsonb jsonb = JsonbBuilder.create(cfg);
+            jsonb.toJson(daia, bw);
+        }
+    }
+
+    public String marshalToString(DAIA daia) throws Exception {
+        StringWriter sw = new StringWriter();
+        Jsonb jsonb = JsonbBuilder.create(cfg);
+        jsonb.toJson(daia, sw);
+        return sw.toString();
+    }
+
+    public DAIA unmarshal(String s) throws Exception {
+        Jsonb jsonb = JsonbBuilder.create(cfg);
+        return jsonb.fromJson(new StringReader(s), DAIA.class);
+    }
+
+    public DAIA unmarshal(Path p) throws Exception {
+        try (BufferedReader br = Files.newBufferedReader(p)){
+            Jsonb jsonb = JsonbBuilder.create(cfg);
+            return jsonb.fromJson(br, DAIA.class);
+        }
+        catch(Exception e) {
+            //do nothing
+        }
+        return null;
+    }
+
+    public DAIA unmarshal(URL url) throws Exception {
+        try (InputStream is = url.openStream()){
+            Jsonb jsonb = JsonbBuilder.create(cfg);
+            return jsonb.fromJson(is, DAIA.class);
+        }
+    }
+}
