4eebe9f699d82565e1b77c848cd9cccb348529af
Robert Stephan <robert.stephan@uni-rostock.de>
2023-11-26 13:05:26 +0100

    add Agent class and extract NameDefinition from Name #19

---
+++ libmeta-mods/src/main/java/org/mycore/libmeta/mods/model/_misc/builder/INameDefinitionBuilder.java
@@ -0,0 +1,76 @@
+/* 
+ * This file is part of *** MyCoRe LibMeta ***
+ * See https://github.com/MyCoRe-Org/libmeta/ for details.
+ * 
+ * MyCoRe LibMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * MyCoRe LibMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyCoRe LibMeta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.libmeta.mods.model._misc.builder;
+
+import org.mycore.libmeta.mods.model._misc.INameDefinitionSubelement;
+import org.mycore.libmeta.mods.model._misc.NameDefinition;
+import org.mycore.libmeta.mods.model._misc.enums.NameType;
+import org.mycore.libmeta.mods.model._misc.enums.Usage;
+import org.mycore.libmeta.xlink.model._builder.IAttributeGroupXlinkSimpleLinkBuilder;
+
+/**
+ * Content: XsString
+ * 
+ * @author Robert Stephan
+ * @version MODS 3.6
+ *
+ */
+public interface INameDefinitionBuilder<T extends NameDefinition, B>
+    extends  IAuthorityAttributeGroupBuilder<NameDefinition, B>,
+    IAttributeGroupXlinkSimpleLinkBuilder<NameDefinition, B>, 
+    ILanguageAttributeGroupBuilder<NameDefinition, B> {
+    
+    public T _target();
+    public B _self();
+    
+    public default B addContent(INameDefinitionSubelement content) {
+        _target().getContent().add(content);
+        return _self();
+    }
+
+    public default B ID(String id) {
+        _target().setID(id);
+        return _self();
+    }
+
+    public default B displayLabel(String displayLabel) {
+        _target().setDisplayLabel(displayLabel);
+        return _self();
+    }
+
+    public default B altRepGroup(String s) {
+        _target().setAltRepGroup(s);
+        return _self();
+    }
+
+    public default B nameTitleGroup(String s) {
+        _target().setNameTitleGroup(s);
+        return _self();
+    }
+
+    public default B usage(Usage usage) {
+        _target().setUsage(usage);
+        return _self();
+    }
+
+    public default B type(NameType type) {
+        _target().setType(type);
+        return _self();
+    }    
+
+}
---
+++ libmeta-mods/src/main/java/org/mycore/libmeta/mods/model/origininfo/Agent.java
@@ -0,0 +1,57 @@
+/* 
+ * This file is part of *** MyCoRe LibMeta ***
+ * See https://github.com/MyCoRe-Org/libmeta/ for details.
+ * 
+ * MyCoRe LibMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * MyCoRe LibMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyCoRe LibMeta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.libmeta.mods.model.origininfo;
+
+import org.mycore.libmeta.common.BuilderBase;
+import org.mycore.libmeta.mods.model._misc.NameDefinition;
+import org.mycore.libmeta.mods.model._misc.builder.INameDefinitionBuilder;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * MODSOriginInfo Subelement agent
+ * 
+ * {@code
+ * <xs:element name="agent" type="nameDefinition"/>
+ * }
+ * 
+ * @author Robert Stephan
+ * @version MODS 3.8
+ *
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+public class Agent extends NameDefinition implements IOriginInfoSubelement {
+
+    public static Builder builder() {
+        return Agent.builder(new Agent());
+    }
+
+    public static Builder builder(Agent agent) {
+        return new Builder(agent);
+    }
+
+    public static class Builder extends BuilderBase<Agent, Builder>
+        implements INameDefinitionBuilder<Agent, Builder> {
+
+        protected Builder(Agent agent) {
+            super(agent);
+        }
+    }
+
+}
