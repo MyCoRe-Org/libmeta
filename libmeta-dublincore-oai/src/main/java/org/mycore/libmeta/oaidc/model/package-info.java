a59c64a9ec30c71994fb7294c189f3bd058561df
Robert Stephan <robert.stephan@uni-rostock.de>
2023-11-28 15:02:08 +0100

    format code #51

--- libmeta-dublincore-oai/src/main/java/org/mycore/libmeta/oaidc/model/package-info.java
+++ libmeta-dublincore-oai/src/main/java/org/mycore/libmeta/oaidc/model/package-info.java
@@ -15,9 +15,10 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with MyCoRe LibMeta.  If not, see <http://www.gnu.org/licenses/>.
  */
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.openarchives.org/OAI/2.0/oai_dc/",
+@XmlSchema(namespace = "http://www.openarchives.org/OAI/2.0/oai_dc/",
     xmlns = { @XmlNs(namespaceURI = "http://www.openarchives.org/OAI/2.0/oai_dc/", prefix = "oai_dc") },
     elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package org.mycore.libmeta.oaidc.model;
 
 import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlSchema;
