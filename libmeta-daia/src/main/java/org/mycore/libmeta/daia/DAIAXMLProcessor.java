ad0329d73a70f8a92d51b3bf30d90f4c54a255df
Steffen Kasten <steffen.kasten@uni-rostock.de>
2023-11-29 12:37:27 +0100

    JUnit test for DAIAXML and remove main() #56

--- libmeta-daia/src/main/java/org/mycore/libmeta/daia/DAIAXMLProcessor.java
+++ libmeta-daia/src/main/java/org/mycore/libmeta/daia/DAIAXMLProcessor.java
@@ -17,39 +17,22 @@
  */
 package org.mycore.libmeta.daia;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.mycore.libmeta.common.DefaultXMLProcessor;
-import org.mycore.libmeta.common.LibmetaXMLProcessorException;
 import org.mycore.libmeta.daia.model.DAIA;
 
 public class DAIAXMLProcessor extends DefaultXMLProcessor<DAIA> {
-    public static final String DAI_SCHEMA_LOCATIONS = "http://ws.gbv.de/daia/ https://raw.githubusercontent.com/gbv/daia/master/schemas/daia.xsd";
+    public static final String DAI_SCHEMA_LOCATIONS
+        = "http://ws.gbv.de/daia/ https://raw.githubusercontent.com/gbv/daia/master/schemas/daia.xsd";
 
     private static final DAIAXMLProcessor INSTANCE = new DAIAXMLProcessor();
- 
+
     //private constructor to avoid client applications to use constructor
     private DAIAXMLProcessor() {
         super(DAIA.class);
     }
-    
+
     public static DAIAXMLProcessor getInstance() {
         return INSTANCE;
     }
-    
 
-    public static void main(String[] args) {
-        try {
-            DAIA daia = DAIAXMLProcessor.getInstance().unmarshal(new URL("http://daia.gbv.de/?format=xml&id=opac-de-28%3Appn%3A56147334X"));
-            System.out.println(daia.getTimestamp());
-            System.out.println(daia.getDocument().get(0).getItem().get(0).getAvailable().get(0).getHref());
-
-            DAIA daia2 = DAIAXMLProcessor.getInstance().unmarshal(new URL("http://daia.gbv.de/?format=xml&id=opac-de-28%3Appn%3A782683118"));
-            System.out.println(daia2.getTimestamp());
-
-        } catch (LibmetaXMLProcessorException | MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
 }
