fbde69ee5c598fdb22c7fc2a7aad9e9bc2334466
Steffen Kasten <steffen.kasten@uni-rostock.de>
2023-12-13 15:30:38 +0100

    add test for cartographics #34

--- libmeta-mods/src/test/java/org/mycore/libmeta/mods/junit5/model/_toplevel/OriginInfoTest.java
+++ libmeta-mods/src/test/java/org/mycore/libmeta/mods/junit5/model/_toplevel/OriginInfoTest.java
@@ -6,10 +6,13 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 import org.mycore.libmeta.mods.MODSXMLProcessor;
 import org.mycore.libmeta.mods.model.Mods;
+import org.mycore.libmeta.mods.model._misc.CodeOrText;
 import org.mycore.libmeta.mods.model._misc.DateEncoding;
 import org.mycore.libmeta.mods.model._misc.enums.NameType;
 import org.mycore.libmeta.mods.model._misc.enums.Yes;
 import org.mycore.libmeta.mods.model._toplevel.OriginInfo;
+import org.mycore.libmeta.mods.model.cartographics.Cartographics;
+import org.mycore.libmeta.mods.model.cartographics.Coordinates;
 import org.mycore.libmeta.mods.model.name.NamePart;
 import org.mycore.libmeta.mods.model.name.Role;
 import org.mycore.libmeta.mods.model.name.RoleTerm;
@@ -81,6 +84,47 @@ public class OriginInfoTest {
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+    
+    @Test
+    public void test2() {
+        Mods mods = Mods.builder()
+            .addContent(OriginInfo.builderForOriginInfo()
+                .eventType("publication")
+                .addContent(Place.builderForPlace()
+                    .addContent(PlaceTerm.builderForPlaceTerm()
+                        .type(CodeOrText.TEXT)
+                        .content("Rostock")
+                        .build())
+                    .addContent(Cartographics.builder()
+                        .authority("ISO 6709")
+                        .addCoordinates(Coordinates.builder()
+                            .content("+54.0755+012.1035")
+                            .build())
+                        .build())
+                    .build())
+                .build())
+            .build();
+        try {
+            String s = MODSXMLProcessor.getInstance().marshalToString(mods);
+            System.out.println(s);
+            String expected =
+                  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                  + "<mods:mods xmlns:mods=\"http://www.loc.gov/mods/v3\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n"
+                  + "  <mods:originInfo eventType=\"publication\">\n"
+                  + "    <mods:place>\n"
+                  + "      <mods:placeTerm type=\"text\">Rostock</mods:placeTerm>\n"
+                  + "      <mods:cartographics authority=\"ISO 6709\">\n"
+                  + "        <mods:coordinates>+54.0755+012.1035</mods:coordinates>\n"
+                  + "      </mods:cartographics>\n"
+                  + "    </mods:place>\n"
+                  + "  </mods:originInfo>\n"
+                  + "</mods:mods>";
+            assertEquals("Test 2 failed", expected, s);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
     }
     
 }
