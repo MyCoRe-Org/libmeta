1c692292bb4c6c8a8625ae59177650f0ec3bc4f8
Steffen Kasten <steffen.kasten@uni-rostock.de>
2024-05-13 12:50:00 +0200

    add stream/listFilteredContent in MODSQuery and add tests #69

--- libmeta-mods/src/main/java/org/mycore/libmeta/mods/MODSQuery.java
+++ libmeta-mods/src/main/java/org/mycore/libmeta/mods/MODSQuery.java
@@ -26,10 +26,25 @@ import org.mycore.libmeta.mods.model._misc.IAlternativeNameSubelement;
 import org.mycore.libmeta.mods.model._misc.INameSubelement;
 import org.mycore.libmeta.mods.model._toplevel.ITopLevelElement;
 import org.mycore.libmeta.mods.model._toplevel.Name;
+import org.mycore.libmeta.mods.model._toplevel.OriginInfo;
+import org.mycore.libmeta.mods.model._toplevel.Part;
+import org.mycore.libmeta.mods.model._toplevel.PhysicalDescription;
 import org.mycore.libmeta.mods.model._toplevel.RecordInfo;
+import org.mycore.libmeta.mods.model._toplevel.Subject;
+import org.mycore.libmeta.mods.model._toplevel.TitleInfo;
 import org.mycore.libmeta.mods.model.name.AlternativeName;
 import org.mycore.libmeta.mods.model.origininfo.Agent;
+import org.mycore.libmeta.mods.model.origininfo.IOriginInfoSubelement;
+import org.mycore.libmeta.mods.model.origininfo.Place;
+import org.mycore.libmeta.mods.model.part.Detail;
+import org.mycore.libmeta.mods.model.part.IPartSubelement;
+import org.mycore.libmeta.mods.model.physicaldescription.IPhysicalDescriptionSubelement;
 import org.mycore.libmeta.mods.model.recordinfo.IRecordInfoSubelement;
+import org.mycore.libmeta.mods.model.subject.ISubjectSubelement;
+import org.mycore.libmeta.mods.model.subject.SubjectHierarchicalGeographic;
+import org.mycore.libmeta.mods.model.subject.SubjectName;
+import org.mycore.libmeta.mods.model.subject.SubjectTitleInfo;
+import org.mycore.libmeta.mods.model.titleInfo.ITitleInfoSubelement;
 
 public class MODSQuery {
     /**
@@ -134,4 +149,205 @@ public class MODSQuery {
     public static <T extends IAgentSubelement> List<T> listFilteredContent(Agent agent, Class<T> type) {
         return streamFilteredContent(agent, type).toList();
     }
+    
+    /**
+     * returns a filtered List of OriginInfoSubelement
+     * @param <T> the type of the OriginInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS originmInfo subelements
+     */
+    public static <T extends IOriginInfoSubelement> Stream<T> streamFilteredContent(OriginInfo originInfo, Class<T> type) {
+        return originInfo.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of OriginInfoSubelement
+     * @param <T> the type of the OriginInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS origin Info subelements
+     */
+    public static <T extends IOriginInfoSubelement> List<T> listFilteredContent(OriginInfo originInfo, Class<T> type) {
+        return streamFilteredContent(originInfo, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of PartSubelement
+     * @param <T> the type of the PartSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS part subelements
+     */
+    public static <T extends IPartSubelement> Stream<T> streamFilteredContent(Part part, Class<T> type) {
+        return part.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of PartSubelement
+     * @param <T> the type of the PartSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS part Info subelements
+     */
+    public static <T extends IPartSubelement> List<T> listFilteredContent(Part part, Class<T> type) {
+        return streamFilteredContent(part, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of PhysicalDescriptionSubelement
+     * @param <T> the type of the PhysicalDescriptionSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS physical description subelements
+     */
+    public static <T extends IPhysicalDescriptionSubelement> Stream<T> streamFilteredContent(PhysicalDescription physicalDescription, Class<T> type) {
+        return physicalDescription.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of PhysicalDescriptionSubelement
+     * @param <T> the type of the PhysicalDescriptionSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS physical description subelements
+     */
+    public static <T extends IPhysicalDescriptionSubelement> List<T> listFilteredContent(PhysicalDescription physicalDescription, Class<T> type) {
+        return streamFilteredContent(physicalDescription, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of SubjectSubelement
+     * @param <T> the type of the SubjectSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject subelements
+     */
+    public static <T extends ISubjectSubelement> Stream<T> streamFilteredContent(Subject subject, Class<T> type) {
+        return subject.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of SubjectSubelement
+     * @param <T> the type of the SubjectSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject subelements
+     */
+    public static <T extends ISubjectSubelement> List<T> listFilteredContent(Subject subject, Class<T> type) {
+        return streamFilteredContent(subject, type).toList();
+    }
+
+    /**
+     * returns a filtered List of TitleInfoSubelement
+     * @param <T> the type of the TitleInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS title info subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(TitleInfo titleInfo, Class<T> type) {
+        return titleInfo.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of TitleInfoSubelement
+     * @param <T> the type of the TitleInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS title info subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(TitleInfo titleInfo, Class<T> type) {
+        return streamFilteredContent(titleInfo, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of PlaceSubelement
+     * @param <T> the type of the PlaceSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS place subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(Place place, Class<T> type) {
+        return place.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of PlaceSubelement
+     * @param <T> the type of the PlaceSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS place subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(Place place, Class<T> type) {
+        return streamFilteredContent(place, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of PartDetailSubelement
+     * @param <T> the type of the PartDetailSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS part detail subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(Detail detail, Class<T> type) {
+        return detail.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of PartDetailSubelement
+     * @param <T> the type of the PartDetailSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS part detail subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(Detail detail, Class<T> type) {
+        return streamFilteredContent(detail, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of HierarchicalGeographicSubelement
+     * @param <T> the type of the HierarchicalGeographicSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS hierarchical geographic subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(SubjectHierarchicalGeographic subjectHierarchicalGeographic, Class<T> type) {
+        return subjectHierarchicalGeographic.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of HierarchicalGeographicSubelement
+     * @param <T> the type of the HierarchicalGeographicSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS hierarchical geographic subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(SubjectHierarchicalGeographic subjectHierarchicalGeographic, Class<T> type) {
+        return streamFilteredContent(subjectHierarchicalGeographic, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of SubjectNameSubelement
+     * @param <T> the type of the SubjectNameSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject name subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(SubjectName subjectName, Class<T> type) {
+        return subjectName.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of SubjectNameSubelement
+     * @param <T> the type of the SubjectNameSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject name subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(SubjectName subjectName, Class<T> type) {
+        return streamFilteredContent(subjectName, type).toList();
+    }
+    
+    /**
+     * returns a filtered List of SubjectTitleInfoSubelement
+     * @param <T> the type of the SubjectTitleInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject title info subelements
+     */
+    public static <T extends ITitleInfoSubelement> Stream<T> streamFilteredContent(SubjectTitleInfo subjectTitleInfo, Class<T> type) {
+        return subjectTitleInfo.getContent().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered List of SubjectTitleInfoSubelement
+     * @param <T> the type of the SubjectTitleInfoSubelement
+     * @param type the class to filter and cast elements for
+     * @return an object stream of MODS subject name subelements
+     */
+    public static <T extends ITitleInfoSubelement> List<T> listFilteredContent(SubjectTitleInfo subjectTitleInfo, Class<T> type) {
+        return streamFilteredContent(subjectTitleInfo, type).toList();
+    }
+    
 }
---
+++ libmeta-mods/src/test/java/org/mycore/libmeta/mods/junit5/model/MODSQueryTest.java
@@ -0,0 +1,138 @@
+package org.mycore.libmeta.mods.junit5.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.mycore.libmeta.common.LibmetaProcessorException;
+import org.mycore.libmeta.mods.MODSQuery;
+import org.mycore.libmeta.mods.MODSXMLProcessor;
+import org.mycore.libmeta.mods.model.Mods;
+import org.mycore.libmeta.mods.model._misc.enums.Usage;
+import org.mycore.libmeta.mods.model._toplevel.Extension;
+import org.mycore.libmeta.mods.model._toplevel.Name;
+import org.mycore.libmeta.mods.model._toplevel.RecordInfo;
+import org.mycore.libmeta.mods.model._toplevel.TitleInfo;
+import org.mycore.libmeta.mods.model.name.Affiliation;
+import org.mycore.libmeta.mods.model.recordinfo.RecordContentSource;
+import org.mycore.libmeta.mods.model.recordinfo.RecordIdentifier;
+import org.mycore.libmeta.mods.model.recordinfo.RecordOrigin;
+import org.mycore.libmeta.mods.model.titleInfo.Title;
+import org.w3c.dom.Node;
+
+public class MODSQueryTest {
+
+    @Test
+    public void test1() {
+        String expected = """
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+              <mods:mods xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                <mods:extension type="degree">
+                  <edtms>
+                    <name>Ph.D.</name>
+                    <level>2</level>
+                    <discipline>Comparitive and International Education</discipline>
+                    <grantor>Coumbia University</grantor>
+                  </edtms>
+                </mods:extension>
+              </mods:mods>
+            """;
+
+        Mods mods;
+        try {
+            mods = MODSXMLProcessor.getInstance().unmarshal(expected);
+            Extension extension = MODSQuery.streamFilteredContent(mods, Extension.class).findFirst().get();
+            Node node = (Node) extension.getContent().stream().filter(Node.class::isInstance).findFirst().get();
+            assertEquals("Test 1 failed", node.getLocalName(), "edtms");
+        } catch (LibmetaProcessorException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void test2() {
+        /*
+        <recordInfo usage="primary">
+          <recordContentSource authority="marcorg">MH</recordContentSource>
+          <recordContentSource authority="marcorg">830804</recordContentSource>
+          <recordContentSource authority="marcorg">20080122</recordContentSource>
+          <recordIdentifier source="ALMA">990000035150203941_FHCL.HOUGH:37522572</recordIdentifier>
+          <recordOrigin>Converted from MARCXML to MODS version 3.8</recordOrigin>
+        </recordInfo>
+        */
+
+        Mods mods = Mods.builder()
+            .addContent(RecordInfo.builderForRecordInfo()
+                .usage(Usage.PRIMARY)
+                .addContent(RecordContentSource.builder()
+                    .authority("marcorg")
+                    .content("MH")
+                    .build())
+                .addContent(RecordContentSource.builder()
+                    .authority("marcorg")
+                    .content("830804")
+                    .build())
+                .addContent(RecordContentSource.builder()
+                    .authority("marcorg")
+                    .content("20080122")
+                    .build())
+                .addContent(RecordIdentifier.builderForRecordIdentifier()
+                    .source("ALMA")
+                    .content("990000035150203941_FHCL.HOUGH:37522572")
+                    .build())
+                .addContent(RecordOrigin.builder()
+                    .content("Converted from MARCXML to MODS version 3.8")
+                    .build())
+                .build())
+            .build();
+
+        RecordInfo recordInfo = MODSQuery.streamFilteredContent(mods, RecordInfo.class).findFirst().get();
+        assertEquals("Test 2 failed", MODSQuery.listFilteredContent(recordInfo, RecordContentSource.class).size(), 3);
+    }
+
+    @Test
+    public void test3() {
+        String r = """
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+              <mods:mods xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+                <mods:titleInfo>
+                  <mods:title>Bush Cheney</mods:title>
+                </mods:titleInfo>
+              </mods:mods>
+            """;
+
+        Mods mods;
+        try {
+            mods = MODSXMLProcessor.getInstance().unmarshal(r);
+            TitleInfo titleInfo = MODSQuery.streamFilteredContent(mods, TitleInfo.class).findFirst().get();
+            Title title = MODSQuery.listFilteredContent(titleInfo, Title.class).get(0);
+            assertEquals("Test 3 failed", title.getContent(), "Bush Cheney");
+        } catch (LibmetaProcessorException e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void test4() {
+        String expected = """
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+              <mods:mods xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+              <mods:name authority="naf" authorityURI="http://id.loc.gov/authorities/names" type="personal" valueURI="http://id.loc.gov/authorities/names/n79022889">
+                <mods:namePart>Einstein, Albert, 1879-1955</mods:namePart>
+                <mods:affiliation authority="ROR" authorityURI="https://ror.org" valueURI="https://ror.org/00f809463">Institute for Advance Study</mods:affiliation>
+              </mods:name>
+            </mods:mods>
+            """;
+        try {
+            Mods mods = MODSXMLProcessor.getInstance().unmarshal(expected);
+            Name name = MODSQuery.streamFilteredContent(mods, Name.class).findFirst().get();
+            Affiliation affiliation = MODSQuery.listFilteredContent(name, Affiliation.class).get(0);
+            assertEquals("Test 4 failed", affiliation.getAuthority(), "ROR");
+        } catch (LibmetaProcessorException e) {
+            fail(e.getMessage());
+        }
+    }
+
+}
