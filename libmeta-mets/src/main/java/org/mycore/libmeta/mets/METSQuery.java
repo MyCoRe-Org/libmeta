af9f4baaaf3a5b3a86df8ad64d0b14a688c1fd70
Steffen Kasten <steffen.kasten@uni-rostock.de>
2024-05-14 14:48:31 +0200

    METS: new methods to filter content elements by class #76

--- libmeta-mets/src/main/java/org/mycore/libmeta/mets/METSQuery.java
+++ libmeta-mets/src/main/java/org/mycore/libmeta/mets/METSQuery.java
@@ -17,15 +17,21 @@
  */
 package org.mycore.libmeta.mets;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.mycore.libmeta.mets.model.Mets;
 import org.mycore.libmeta.mets.model._enums.LOCTYPE;
+import org.mycore.libmeta.mets.model.div.AreaOrSeqOrPar;
+import org.mycore.libmeta.mets.model.div.Par;
+import org.mycore.libmeta.mets.model.div.Seq;
 import org.mycore.libmeta.mets.model.filesec.File;
 import org.mycore.libmeta.mets.model.filesec.FileGrp;
 import org.mycore.libmeta.mets.model.filesec.FileSec;
 import org.mycore.libmeta.mets.model.filesec.file.FLocat;
 import org.mycore.libmeta.mets.model.mdsec.MdSec;
+import org.mycore.libmeta.mets.model.structlink.SmLinkOrSmLinkGrp;
+import org.mycore.libmeta.mets.model.structlink.StructLink;
 import org.mycore.libmeta.mets.model.structmap.Div;
 import org.mycore.libmeta.mets.model.structmap.StructMap;
 
@@ -163,4 +169,71 @@ public class METSQuery {
     public static Stream<Div> treeTraversalStreamOfDivs(Div div) {
         return Stream.concat(Stream.of(div), div.getDiv().stream().flatMap(x -> treeTraversalStreamOfDivs(x)));
     }
+    
+    /**
+     * returns a filtered stream of SmLinkOrSmLinkGrp
+     * @param <T> the type of the SmLinkOrSmLinkGrp
+     * @param structLink parent of SmLinkOrSmLinkGrp
+     * @param type the class to filter and cast elements for
+     * @return an object stream of SmLinkOrSmLinkGrp
+     */
+    public static <T extends SmLinkOrSmLinkGrp> Stream<T> streamFilteredContent(StructLink structLink, Class<T> type) {
+        return structLink.getSmLinkOrSmLinkGrp().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered list of SmLinkOrSmLinkGrps
+     * @param <T> the type of the SmLinkOrSmLinkGrp
+     * @param structLink parent of SmLinkOrSmLinkGrp
+     * @param type the class to filter and cast elements for
+     * @return an object stream of SmLinkOrSmLinkGrp
+     */
+    public static <T extends SmLinkOrSmLinkGrp> List<T> listFilteredContent(StructLink structLink, Class<T> type) {
+        return streamFilteredContent(structLink, type).toList();
+    }
+    
+    /**
+     * returns a filtered stream of AreaOrSeqOrPar
+     * @param <T> the type of the AreaOrSeqOrPar
+     * @param par parent of AreaOrSeqOrPar
+     * @param type the class to filter and cast elements for
+     * @return an object stream of AreaOrSeqOrPar
+     */
+    public static <T extends AreaOrSeqOrPar> Stream<T> streamFilteredContent(Par par, Class<T> type) {
+        return par.getAreaOrSeq().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered list of AreaOrSeqOrPar
+     * @param <T> the type of the AreaOrSeqOrPar
+     * @param par parent of AreaOrSeqOrPar
+     * @param type the class to filter and cast elements for
+     * @return an object stream of AreaOrSeqOrPar
+     */
+    public static <T extends AreaOrSeqOrPar> List<T> listFilteredContent(Par par, Class<T> type) {
+        return streamFilteredContent(par, type).toList();
+    }
+    
+    /**
+     * returns a filtered stream of AreaOrSeqOrPar
+     * @param <T> the type of the AreaOrSeqOrPar
+     * @param seq parent of AreaOrSeqOrPar
+     * @param type the class to filter and cast elements for
+     * @return an object stream of AreaOrSeqOrPar
+     */
+    public static <T extends AreaOrSeqOrPar> Stream<T> streamFilteredContent(Seq seq, Class<T> type) {
+        return seq.getAreaOrPar().stream().filter(type::isInstance).map(type::cast);
+    }
+
+    /**
+     * returns a filtered list of AreaOrSeqOrPar
+     * @param <T> the type of the AreaOrSeqOrPar
+     * @param seq parent of AreaOrSeqOrPar
+     * @param type the class to filter and cast elements for
+     * @return an object stream of AreaOrSeqOrPar
+     */
+    public static <T extends AreaOrSeqOrPar> List<T> listFilteredContent(Seq seq, Class<T> type) {
+        return streamFilteredContent(seq, type).toList();
+    }
+    
 }
