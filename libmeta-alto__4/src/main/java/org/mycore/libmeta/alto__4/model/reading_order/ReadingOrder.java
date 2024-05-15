828bb5b37f86bc4ec4c01b21174e9d6680638671
Robert Stephan <robert.stephan@uni-rostock.de>
2024-05-15 14:29:14 +0200

    initial check-in alto v4 #61

---
+++ libmeta-alto__4/src/main/java/org/mycore/libmeta/alto__4/model/reading_order/ReadingOrder.java
@@ -0,0 +1,50 @@
+/* 
+ * This file is part of *** MyCoRe LibMeta ***
+ * See https://github.com/MyCoRe-Org/libmeta/ for details.
+ * 
+ * MyCoRe LibMeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * MyCoRe LibMeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyCoRe LibMeta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.mycore.libmeta.alto__4.model.reading_order;
+
+import org.mycore.libmeta.common.BuilderBase;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+
+/**
+ * The root layout element.
+ * 
+ * @author Robert Stephan
+ * @version Alto 2.1
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+public class ReadingOrder {
+
+    public static Builder builder() {
+        return builder(new ReadingOrder());
+    }
+
+    public static Builder builder(ReadingOrder r) {
+        return new Builder(r);
+    }
+
+    
+    public static class Builder extends BuilderBase<ReadingOrder, Builder>  {
+        protected Builder(ReadingOrder r) {
+            super(r);
+        }
+        
+        
+    }
+}
